ec5a13a2bfeec9105d60bf3b25b7e26bcd5d26c5
Alehegne <alehegngetaenyewuu@gmail.com>
2026-05-28 13:08:10 +0300

    adjust db connection

--- src/main/java/config/DBConnection.java
+++ src/main/java/config/DBConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DBConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/finance_tracker";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "password";
+    private static final String USER = "tracker";
+    private static final String PASSWORD = "123456";
 
     private static Connection connection = null;
 
